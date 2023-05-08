07eb629dee90238177ac7a25372edbd66759faba
Zinitor <mr.artyom01@mail.ru>
2023-05-08 20:24:22 +0300

    uploaded files

---
+++ scripts/object_list.js
@@ -0,0 +1,114 @@
+document.addEventListener("DOMContentLoaded", function () {
+  ymaps.ready(init);
+  function init() {
+    // Создание экземпляра карты.
+    var myMap = new ymaps.Map(
+        "map",
+        {
+          center: [56.319962084999545,43.966574413001695],
+          zoom: 12,
+        },
+        {
+          searchControlProvider: "yandex#search",
+        }
+      ),
+      // Контейнер для меню.
+      menu = $('<ul class="menu-filters"> ');
+
+    for (var i = 0, l = groups.length; i < l; i++) {
+      createMenuGroup(groups[i]);
+    }
+
+    function createMenuGroup(group) {
+      // Пункт меню.
+      var menuItem = $('<li><a href="#" class="on">' + '<i class="' + group.icon +'">' + "</a></li>"),
+        // Коллекция для геообъектов группы.
+        collection = new ymaps.GeoObjectCollection(null, {
+          preset: group.style,
+          iconGlyph: group.iconGlyph,
+          iconGlyphColor: group.iconGlyphColor
+        }),
+        // Контейнер для подменю.
+        submenu = $('<ul class="nav-links"></ul>');
+        submenu.hide();
+      // Добавляем коллекцию на карту.
+      myMap.geoObjects.add(collection);
+      // Добавляем подменю.
+      menuItem
+        .append(submenu)
+        // Добавляем пункт в меню.
+        .appendTo(menu)
+        // По клику удаляем/добавляем коллекцию на карту и скрываем/отображаем подменю.
+        .find("a")
+        .bind("click", function () {
+          $(this).toggleClass('on off');
+          if (collection.getParent()) {
+            myMap.geoObjects.remove(collection);
+          } else {
+            myMap.geoObjects.add(collection);
+          }
+        });
+      for (var j = 0, m = group.items.length; j < m; j++) {
+        createSubMenu(group.items[j], collection, submenu, group);
+      }
+    }
+
+    function createSubMenu(item, collection, submenu, group) {
+      // Пункт подменю.
+      var submenuItem = $('<li><a href="#">' + item.name + "</a></li>"),
+        // Создаем метку.
+        placemark = new ymaps.Placemark(item.center, {
+          balloonContent: item.name,
+        });
+      placemark.events.add("click", function () {
+        document.getElementById("info-button").href =
+          "info.html?group=" + encodeURIComponent(group.name) + "&item=" + encodeURIComponent(item.id);
+      });
+
+      // Добавляем метку в коллекцию.
+      collection.add(placemark);
+      // Добавляем пункт в подменю.
+      submenuItem
+        .appendTo(submenu)
+        // При клике по пункту подменю открываем/закрываем баллун у метки.
+        .find("a")
+        .bind("click", function () {
+          if (!placemark.balloon.isOpen()) {
+            placemark.balloon.open();
+          } else {
+            placemark.balloon.close();
+          }
+          return false;
+        });
+    }
+
+    // Добавляем меню в тэг menu.
+    menu.appendTo($(".sidebar .menu-bar .menu"));
+    // Выставляем масштаб карты чтобы были видны все группы.
+    myMap.setBounds(myMap.geoObjects.getBounds());
+
+      //Нахождение юрл параметра
+  var urlParams = new URLSearchParams(window.location.search);
+  var landmarkName = urlParams.get('landmark');
+
+  // Проверка если место вписано в url
+  if (landmarkName) {
+    // Проход по всем группам
+    for (var i = 0, l = groups.length; i < l; i++) {
+      for (var j = 0, m = groups[i].items.length; j < m; j++) {
+        var item = groups[i].items[j];
+        if (item.name === landmarkName) {
+          // Отцентрировать
+          myMap.setCenter(item.center, 16);
+          var placemark = item;
+          break;
+        }
+      }
+      if (placemark) {
+        break;
+      }
+    }
+  }
+  }
+});
+
---
+++ scripts/parseLandmarks.js
@@ -0,0 +1,46 @@
+function goBackToMapButtonUrl() {
+  // Parse the landmark name from the URL parameter
+  const params = new URLSearchParams(window.location.search);
+  const landmarkName = params.get("landmark");
+  document.getElementById("map-button").href =
+  "index.html?landmark=" + encodeURIComponent(landmarkName);
+}
+
+//wait for page to load
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the query string parameters from the URL
+  const queryString = window.location.search;
+
+  // Create a new URLSearchParams object and pass the query string as a parameter
+  const urlParams = new URLSearchParams(queryString);
+
+  // Get the value of the 'landmark' parameter
+
+  // Display the landmark name on the page
+
+  var groupName = urlParams.get("group");
+  var itemid = urlParams.get("item");
+  var filePath = './/' +'groups/' + groupName + '/' + itemid + '.json';
+  $.getJSON(filePath)
+    .done(function(data) {
+        var itemName = data.name;
+        // Use the itemName variable as needed
+    })
+    .fail(function(jqxhr, textStatus, error) {
+        var err = textStatus + ", " + error;
+        console.log("Error loading JSON data: " + err);
+        // Handle the error as needed
+    });
+
+  var landmarkElement = document.getElementById("landmark-title");
+
+  var descriptionElement = document.getElementById("landmark-description");
+  if (landmarkElement) {
+    landmarkElement.innerHTML = data.name;
+  } else {
+    console.log("The element does not exist.");
+  }
+ 
+
+  goBackToMapButtonUrl();
+});
